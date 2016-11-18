410578504f47c9768d1dbfddddcbff0d04df77e3
Jan Horak <jhorak@redhat.com>
2016-11-18 13:55:50 +0100

    inital changes

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 set -ux
 
 rm -rf app
-flatpak-builder --ccache --require-changes --repo=repo --subject="Nightly build of Firefox, `date`" app org.mozilla.firefox.json
+export FLATPAK_BWRAP=/usr/bin/bwrap
+flatpak-builder --verbose --disable-updates --disable-download --ccache --require-changes --repo=repo --subject="Firefox Developer Edition Build" app org.mozilla.firefox-devedition.json
 
 flatpak build-update-repo --prune --prune-depth=20 repo
 
 # The following commands should be performed once
-flatpak --user remote-add --no-gpg-verify nightly-firefox ./repo || true
-flatpak --user -v install nightly-firefox org.mozilla.firefox || true
+flatpak --user remote-add --no-gpg-verify firefox-developer-edition ./repo || true
+flatpak --user -v install firefox-developer-edition org.mozilla.firefox-devedition || true
 
-flatpak update --user org.mozilla.firefox
+flatpak update --user org.mozilla.firefox-devedition
