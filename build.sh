e80ae115587b65b38ad0838639f44ea4aab7f935
Jan Horak <xhorak@gmail.com>
2017-01-10 19:50:17 +0100

    - Removed without-intl-api (fixing wrong context menu) - Pruning repo

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -uex
-rm -rf app repo
+#rm -rf app repo
+rm -rf app
 APP_NAME=org.mozilla.FirefoxDevEdition
 
 # Build repo
 flatpak-builder $GPG_SETTINGS --verbose --force-clean --ccache --require-changes --keep-build-dirs --repo=repo --subject="Firefox Developer Edition Build" app $APP_NAME.json
+
+# Get rid of old files in repo
+flatpak build-update-repo $GPG_SETTINGS --prune --prune-depth=2 repo
 
 # Build .flatpak file
 flatpak build-bundle repo $APP_NAME.flatpak $APP_NAME
