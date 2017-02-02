683ac57e838b082fb4000ede0260d5da24286651
Joaquim Rocha <jrocha@endlessm.com>
2017-02-02 21:57:18 +0100

    Use Firefox's binaries from extra-data instead of compiling them
    This is needed in order to comply with the Mozilla trademark's terms
    of use. This also means that most of the compiled sources were removed,
    making the build much faster.
    
    Because now Firefox's executable will be placed in a different path
    (/app/extra/...), this patch also changes the command from
    'firefox-wrapper' to simply 'firefox'.
    
    https://phabricator.endlessm.com/T14332

--- firefox.sh
+++ firefox.sh
@@ -13,4 +13,4 @@ export PATH="$HOME/.flatpak_extras/firefox/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/.flatpak_extras/firefox/lib/amd64:$HOME/.flatpak_extras/firefox/lib/amd64/jli:$LD_LIBRARY_PATH"
 export JAVA_HOME="$Home/.flatpak_extras/firefox"
 
-exec /app/bin/firefox $@
+exec /app/extra/firefox/firefox-bin $@
