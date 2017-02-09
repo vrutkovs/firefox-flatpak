1794a8610ddaf701082fd11b85d6e4476cd1b998
Roddy Shuler <roddy@endlessm.com>
2017-02-08 22:04:26 -0800

    firefox.sh: avoid calling firefox-bin twice
    It seems that the intention was to simply move the launch
    of firefox-bin after firefox-plugins-installer, not to make
    a copy of it and run it before and after the plugins installer.
    
    https://phabricator.endlessm.com/T14332

--- firefox.sh
+++ firefox.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-exec /app/extra/firefox/firefox-bin $@
 # Launch the script that installs the plugins (if needed) in the background
 # so it installs them while Firefox is already running, avoiding the user
 # to wait
