1df394b79d479d16576d121851c8bd2cad84e234
Jan Horak <xhorak@gmail.com>
2017-02-17 12:43:19 +0100

    Support for GPG_BUNDLE_SETTINGS and don't keep build dirs

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ fi
 
 
 # Build repo
-flatpak-builder $GPG_SETTINGS --verbose --force-clean --ccache --require-changes --keep-build-dirs --repo=repo --subject="Build of $APP_NAME" app $APP_NAME/$APP_NAME.json
+flatpak-builder $GPG_SETTINGS --verbose --force-clean --ccache --require-changes --repo=repo --subject="Build of $APP_NAME" app $APP_NAME/$APP_NAME.json
 
 # Get rid of old files in repo
 flatpak build-update-repo $GPG_SETTINGS --prune --prune-depth=2 repo
 
 # Build .flatpak file
-flatpak build-bundle repo $APP_NAME.flatpak $APP_NAME
+flatpak build-bundle $GPG_BUNDLE_SETTINGS repo $APP_NAME.flatpak $APP_NAME
