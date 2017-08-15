60f53b6ffe45103f89605c3b69dc10cb564aa800
Jan Horak <xhorak@gmail.com>
2017-08-15 09:47:41 +0200

    Save some space in the repo by keeping only previous ostree version

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ fi
 flatpak-builder $GPG_SETTINGS --verbose --force-clean --ccache --require-changes --repo=repo --subject="Build of $APP_NAME" app $APP_NAME/$APP_NAME.json
 
 # Get rid of old files in repo
-flatpak build-update-repo $GPG_SETTINGS --prune --prune-depth=2 repo
+flatpak build-update-repo $GPG_SETTINGS --prune --prune-depth=1 repo
 
 # Build .flatpak file
 flatpak build-bundle $GPG_BUNDLE_SETTINGS repo $APP_NAME.flatpak $APP_NAME
