caf0e5ec2004278b9e13d761e910a98f1f761655
Jan Horak <xhorak@gmail.com>
2018-02-09 08:56:42 +0100

    - Adding gnome 3.26 runtime, removing obsolete ones

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -4,11 +4,8 @@ set -ux
 curl -O https://sdk.gnome.org/keys/gnome-sdk.gpg
 flatpak remote-add --gpg-import=gnome-sdk.gpg gnome https://sdk.gnome.org/repo/
 
-flatpak install gnome org.gnome.Platform 3.20
-flatpak install gnome org.gnome.Sdk 3.20
-
-flatpak install gnome org.gnome.Platform 3.22
-flatpak install gnome org.gnome.Sdk 3.22
-
 flatpak install gnome org.gnome.Platform 3.24
 flatpak install gnome org.gnome.Sdk 3.24
+
+flatpak install gnome org.gnome.Platform 3.26
+flatpak install gnome org.gnome.Sdk 3.26
