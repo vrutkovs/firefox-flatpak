e5daba4ce3b7fb8cc5b187e1dc75cb6f6cb95456
Jan Horak <xhorak@gmail.com>
2018-11-13 14:28:24 +0100

    - Fixed firefox launch script - added own dbus

--- org.mozilla.FirefoxNightly/firefox.sh
+++ org.mozilla.FirefoxNightly/firefox.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-export TMPDIR=$$XDG_CACHE_HOME/tmp
-
+export TMPDIR=$XDG_CACHE_HOME/tmp
 if [ "$WAYLAND_DISPLAY" ]; then
   export GDK_BACKEND=wayland
 else
   export GDK_BACKEND=x11
 fi
-firefox-bin $$*
+firefox-bin $*
 
