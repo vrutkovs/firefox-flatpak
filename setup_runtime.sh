ceb36007cfa3925398d867b84f32e68a6b93a44c
Jan Horak <jhorak@redhat.com>
2019-11-12 13:44:37 +0100

    Use gnome runtime 3.32, update nasm package

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -3,6 +3,6 @@ set -ux
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub org.gnome.Platform//3.24 org.gnome.Sdk//3.24
-flatpak install flathub org.gnome.Platform//3.26 org.gnome.Sdk//3.26
+flatpak install flathub org.gnome.Platform//3.32 org.gnome.Sdk//3.32
+#flatpak install flathub org.gnome.Platform//3.26 org.gnome.Sdk//3.26
 flatpak install flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
